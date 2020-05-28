d59177910b3babe2bf3399942577d5d40a4bedd2
kaiba <kaiba@192.168.100.9>
2020-05-28 22:35:16 +0530

    Maven_Integration_Project

---
+++ Maven_Integration/src/test/java/test/NewTest.java
@@ -0,0 +1,27 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class NewTest 
+{
+	@Test
+  public void first() 
+  {
+		System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+	    driver.get("http://demo.guru99.com/test/guru99home/");  
+		String title = driver.getTitle();				 
+		Assert.assertTrue(title.contains("Demo Guru99 Page"));
+		System.out.println(driver.getTitle());
+		driver.quit();
+  }
+ 
+}
+
