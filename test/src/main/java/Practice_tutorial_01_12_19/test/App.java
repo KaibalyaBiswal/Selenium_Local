9855e6e669db4351ba4248f9c550bdcf714d8c3c
kaiba <kaiba@DESKTOP-O0KLTCN>
2020-05-29 19:07:32 +0530

    test_Project

---
+++ test/src/main/java/Practice_tutorial_01_12_19/test/App.java
@@ -0,0 +1,13 @@
+package Practice_tutorial_01_12_19.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
